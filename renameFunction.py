17a0b635ad73ccb9530513a1b42e52e8541a2138
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2025-03-11 18:35:06 +0100

    Create renameFunction.py
    renamefunction with dataframe

---
+++ renameFunction.py
@@ -0,0 +1,102 @@
+# Any structure that has rows and columns is a data frame for pandas
+
+# Creating Data frame using 2-D list
+import pandas as pd
+
+student_data =[
+    [100,80,10],
+    [90,70,7],
+    [120,100,14],
+    [80,50,2]
+
+
+
+]
+colum=['iq','marks','package']
+obj = pd.DataFrame(student_data,columns=colum)
+
+print(obj)
+
+#     iq  marks  package
+# 0  100     80       10
+# 1   90     70        7
+# 2  120    100       14
+# 3   80     50        2
+
+print("-----------------------------------------------------------------------------------")
+
+# creating Data Frame using Dictionary
+
+stu_dict = {
+    'iq':[100,90,120,80],
+    'marks':[80,70,100,50],
+    'package':[10,7,14,2]
+
+
+}
+
+students =pd.DataFrame(stu_dict)
+print(students)
+
+print(students.rename(columns={'marks':'percentage','package':'LPA'})) # not a permanent change
+print(students)
+print("-----------------------------------------------------------------------------------")
+print(students.rename(columns={'marks':'percentage','package':'LPA'},inplace=True)) # for a permanent change
+print(students)
+
+
+
+# output
+
+#     iq  marks  package
+# 0  100     80       10
+# 1   90     70        7
+# 2  120    100       14
+# 3   80     50        2
+# -----------------------------------------------------------------------------------
+#     iq  marks  package
+# 0  100     80       10
+# 1   90     70        7
+# 2  120    100       14
+# 3   80     50        2
+#     iq  percentage  LPA
+# 0  100          80   10
+# 1   90          70    7
+# 2  120         100   14
+# 3   80          50    2
+#     iq  marks  package
+# 0  100     80       10
+# 1   90     70        7
+# 2  120    100       14
+# 3   80     50        2
+# -----------------------------------------------------------------------------------
+# None
+#     iq  percentage  LPA
+# 0  100          80   10
+# 1   90          70    7
+# 2  120         100   14
+# 3   80          50    2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
